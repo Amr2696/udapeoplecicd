07aed4bb825c5f8c43023a1335b6c5f92878d851
amrsaeed <amrsaeed278@gmail.com>
2023-01-19 19:10:28 +0200

    bishhhh

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,6 +10,7 @@ import { TransformInterceptor } from './modules/common/interceptors/TransformInt
 import * as express from 'express';
 import { ErrorFilter } from './modules/errors/error.filter';
 
+//console.log('Heloo from UdaPeople!');
 async function bootstrap() {
   const logger = new AppLogger();
   logger.info(`NodeJs Version ${process.version}`);
@@ -28,7 +29,7 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('customTag')
     .setBasePath(apiVersionPrefix)
-    .addBearerAuth() // here is an intentional compile error. Remove the "x" and the backend should compile.
+    .addBearerAuth()// here was an intentional compile error. Removed the "x" and the backend compiles.
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup(`api/${apiVersionPrefix}`, app, document);
